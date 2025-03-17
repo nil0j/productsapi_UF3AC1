ac359922b3ca475a7f2abafe34a811595b5fe53f
niliaranet <niliara@proton.me>
2025-03-17 16:14:45 +0100

    update

--- db/mongodb-connection.js
+++ db/mongodb-connection.js
@@ -1,9 +1,13 @@
-const {MongoClient} = require("mongodb");
-const {mongoURI} = require("../config/config.js");
+
+
+const { MongoClient } = require("mongodb");
+// const {mongoURI} = require("../config/config.js");
+
+mongoURI = process.env.MONGO_URI
 
 const dbName = 'products';
 
 // create mongoDB client
-const client = new MongoClient(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = client;
